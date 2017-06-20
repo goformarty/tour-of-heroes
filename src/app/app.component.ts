b38e54aef7f351c7f4c940bc2d647b42f7d41494
Marty <marty.stepien@gmail.com>
2017-06-20 17:29:20 +0100

    provider for hero service component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,12 +65,23 @@ import { HeroService } from './hero.service';
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
